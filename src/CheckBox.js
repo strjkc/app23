ec3cc6b081848b21c3b532bff30eb1d594792483
strahinja <strahinja@Ubuntu.strahinja.ubuntu>
2023-04-17 16:32:55 +0100

    change checkbox accent color"
    ;

--- src/CheckBox.js
+++ src/CheckBox.js
@@ -2,7 +2,7 @@ const CheckBox = ({inputName, state, setState}) => {
     return(
         <div style={{display: "flex", padding: "8px", width: "90%", position:"relative", zIndex:'0', color: "black"}}>
             <p style={{marginRight: "8px"}} className={`input_name ${inputName}_name`}>{inputName}</p>
-            <input  className={`input`} type="checkbox" checked={state} value={state} onChange={setState}></input>
+            <input style={{accentColor: "#817df7"}}  className={`input`} type="checkbox" checked={state} value={state} onChange={setState}></input>
         </div>
     )
 }
